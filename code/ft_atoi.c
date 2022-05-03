a1f91449fe1e673dc5398e269850814134c1aad7
Philipp Aubert <paubert@c1s7d33.42wolfsburg.de>
2022-05-03 15:12:54 +0200

    Add ft_atoi. It's field tested

---
+++ code/ft_atoi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	sign = 1;
+	result = 0;
+	while ((*str >= 9 && *str <= 13) || (*str == 32))
+	{
+		str++;
+	}
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= 48 && *str <= 57)
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (result * sign);
+}
