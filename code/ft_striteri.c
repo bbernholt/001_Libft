93f5b9b41786bc52c31e162bc85e543164179b0a
bbernhol <bbernhol@student.42wolfsburg.de>
2022-05-05 18:59:44 +0200

    ft_striteri - v01

---
+++ code/ft_striteri.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbernhol <bbernhol@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/29 22:40:54 by bbernhol          #+#    #+#             */
+/*   Updated: 2022/05/05 18:44:36 by bbernhol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    unsigned int i;
+
+    i = 0;
+    while (s[i] != '\0')
+    {
+        //printf("Wert von i: %d \n", i);
+        //printf("Addresse von s[%d]: %p \n", i, &s[i]);
+        (*f)(i, &s[i]); //&s[0] is address of fort index of the string "s"
+        i++;
+    }
+}
