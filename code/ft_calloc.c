277b0dd3a048b079672d4c302979bc8baaf1fe55
bbernhol <bbernhol@student.42wolfsburg.de>
2022-06-23 17:06:55 +0200

    if n == 0

--- code/ft_calloc.c
+++ code/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bbernhol <bbernhol@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 07:49:02 by bbernhol          #+#    #+#             */
-/*   Updated: 2022/06/06 17:44:33 by bbernhol         ###   ########.fr       */
+/*   Updated: 2022/06/23 16:45:26 by bbernhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,16 @@ void	*ft_calloc(unsigned int n, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
+	ptr_return = NULL;
+	if (n == 0 || size == 0)
+		return (ptr_return);
 	ptr_return = malloc(n * size);
 	if (ptr_return == NULL)
 		return (ptr_return);
 	else
 	{
 		ptr_buffer = (char *)ptr_return;
-		while (i < n)
+		while (i < n * size)
 		{
 			*ptr_buffer = '\0';
 			ptr_buffer++;
