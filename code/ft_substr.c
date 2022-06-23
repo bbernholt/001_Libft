71c502740dedfddd5b62d6bff4ef7a53765e76a8
bbernhol <bbernhol@student.42wolfsburg.de>
2022-06-23 20:01:07 +0200

    start >=ft_strlen(s)

--- code/ft_substr.c
+++ code/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: bbernhol <bbernhol@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 09:31:45 by bbernhol          #+#    #+#             */
-/*   Updated: 2022/06/07 13:47:32 by bbernhol         ###   ########.fr       */
+/*   Updated: 2022/06/23 20:00:21 by bbernhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,23 @@
 
 char	*ft_substr(char const *s, unsigned int start, unsigned int len)
 {
-	int		i;
-	char	*ptr_return;
+	int				i;
+	unsigned int 	len_s;
+	char			*ptr_return;
 
 	i = 0;
+	ptr_return = NULL;
+	len_s = ft_strlen(s);
+	if (!s)
+		return (ptr_return);
+	if (len_s <= start)
+	{
+		ptr_return = (char *)malloc(1);
+		if (!ptr_return)
+			return (ptr_return);
+		ptr_return[i] = '\0';
+		return (ptr_return);
+	}
 	ptr_return = (char *)malloc(len + 1);
 	if (ptr_return != NULL)
 	{
